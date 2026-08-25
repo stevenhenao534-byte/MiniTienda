a594a451176b70119905fad616def97cddd2e3f9
stevhen <henaogarciasteven@gmail.com>
2026-08-24 20:46:26 -0500

    feat: add product management

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+namespace MiniTiendaLicores.Models;
+
+public class Product
+{
+    public int Id { get; }
+    public string Name { get; }
+    public decimal Price { get; }
+
+    public Product(int id, string name, decimal price)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+    }
+}
